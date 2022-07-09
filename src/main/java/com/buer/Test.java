229c331965b13f10e7bcd7e64aed7d4f7f9df9f6
buer <1420070846@qq.com>
2022-07-09 11:28:19 +0800

    1.0

---
+++ src/main/java/com/buer/Test.java
@@ -0,0 +1,8 @@
+package com.buer;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+        System.out.println("hello git~");
+    }
+}
